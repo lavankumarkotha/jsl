2924106fb304778ad4cc711a4a8675289296c5a9
LAVAN KUMAR KOTHA <lavankumar3004@gmail.com>
2024-04-07 13:59:57 -0500

    This my local Jenkins JSL

---
+++ vars/buildImage.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+def call(String imageName) {
+    echo "Building the docker image"
+    withCredentials([
+            usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')
+    ]){
+        sh "docker build -t $imageName ."
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "docker push $imageName"
+    }
+}
---
+++ vars/deployApp.groovy
@@ -0,0 +1,4 @@
+#!/usr/bin/env groovy
+def call() {
+    echo "Hi this is the deployment step"
+}
